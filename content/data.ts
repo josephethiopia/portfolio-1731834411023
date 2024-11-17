a5e1919937b70b699d6fadc1bd8c920ab24f86b3
Joseph___ <118039462+josephethiopia@users.noreply.github.com>
2024-11-17 12:07:01 +0300

    Add content/data.ts

---
+++ content/data.ts
@@ -0,0 +1,89 @@
+
+export const content = {
+  "home": {
+    "name": "yoseph ashenafi",
+    "description": "Passionated fullstack developer",
+    "location": "Jimma",
+    "availability": "Available",
+    "image": {
+      "src": "",
+      "alt": "yoseph ashenafi"
+    },
+    "socialLinks": {
+      "github": null,
+      "twitter": null,
+      "dribbble": null
+    }
+  },
+  "aboutMe": {
+    "image": {
+      "src": "",
+      "alt": "yoseph ashenafi"
+    },
+    "aboutMe": "Hey , i'm yoseph , passionated fullstack developer. I love solving problems and doing stuff with programming. ",
+    "socialLinks": {
+      "twitter": "https://telegram.com/@joaccount",
+      "github": "https://fb.com/@joe"
+    },
+    "quickFacts": {
+      "column1": [
+        "Over 3 years of experience",
+        "Web Developer"
+      ],
+      "column2": []
+    },
+    "outro": ""
+  },
+  "skills": {
+    "skills": [
+      {
+        "name": "React",
+        "icon": ""
+      },
+      {
+        "name": "Next.js",
+        "icon": ""
+      },
+      {
+        "name": "Zustand",
+        "icon": ""
+      },
+      {
+        "name": "AI",
+        "icon": ""
+      },
+      {
+        "name": "MongoDB",
+        "icon": ""
+      },
+      {
+        "name": "Express",
+        "icon": ""
+      },
+      {
+        "name": "Node.js",
+        "icon": ""
+      },
+      {
+        "name": "Tailwind CSS",
+        "icon": ""
+      },
+      {
+        "name": "shadcn",
+        "icon": ""
+      }
+    ]
+  },
+  "work": {
+    "projects": []
+  },
+  "contact": {
+    "email": "joash.000111@gmail.com",
+    "phone": "0941889660",
+    "header": "Contact Me",
+    "socialLinks": {
+      "facebook": "https://fb.com/@joe",
+      "telegram": "https://telegram.com/@joaccount"
+    }
+  }
+}
